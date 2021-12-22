c0b444bb908d0224680fb285cbae1fb709c11f0f
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-22 00:55:46 -0600

    first commit I believe

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+const Header = () => {
+
+  return (
+    <div>
+      <div>
+        <h1>The Brief, Wondrous Career of DoctorMuch*</h1>
+      </div>
+      <div>
+        <Navigation />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+};
+
+export default Navigation;

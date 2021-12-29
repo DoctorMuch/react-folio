20c760cc1a77b0e075972f2638d1f18ccbf29cef
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-29 13:12:45 -0600

    working on the general project, not just the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Navigation from '../Navigation';
+import headshot from '../../assets/headshot.jpg';
 
 const Header = () => {
 
   return (
     <header className='container'>
       <div className='flex-box'>
-        <h1>David F. Mueth</h1>
-        <h2>(The Brief, Wondrous Career of DoctorMuch*)</h2>
+        <div>
+          <h1>David F. Mueth</h1>
+          <h2>(The Brief, Wondrous Career of DoctorMuch*)</h2>
+        </div>
+        <img src={headshot} alt='David Mueth himself' />
       </div>
       <div>
         <Navigation
-           />
+        />
       </div>
     </header>
   )
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,9 +3,29 @@ import coffeeConnect from '../../assets/Coffee-Connection-Start.png';
 
 const Project = () => {
   return (
-    <div>
-      <h5>Here are some words.</h5>
-      <img src={coffeeConnect} />
+    <div className="container flex-box">
+      <div className="card">
+        <img
+          src={coffeeConnect}
+          alt="Cover from Coffee Connection project"
+          className="card-img"
+        />
+        <h4 className="card-header">Coffee Connection</h4>
+        <p className="card-text">
+          A simple application for a user to find nearby coffee shops, <br /> based on their location.
+        </p>
+        <a
+          href="https://doctormuch.github.io/fs-joe-java-journey/"
+          target="_blank"
+          rel="noreferrer"
+        >See the project here ||
+        </a>
+        <a
+          href="https://github.com/DoctorMuch/fs-joe-java-journey"
+          target="_blank"
+          rel="noreferrer"
+        > Explore the GitHub repository</a>
+      </div>
     </div>
   )
 }
